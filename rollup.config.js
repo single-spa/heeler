d2d1d56a586587e52c91b9db14f16d0459f51f9a
Jolyn <jolyndenning@gmail.com>
2025-09-20 02:41:11 -0600

    Bundle to pin dependencies (#17)

---
+++ rollup.config.js
@@ -0,0 +1,12 @@
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+
+export default {
+  input: "./bin/heeler.js",
+  output: {
+    dir: "dist",
+    format: "es",
+  },
+  plugins: [nodeResolve(), commonjs(), json()],
+};
